1f96bef6107804ba875b3f669ff9075e03ceeeb5
mywebportfolio1 <shepotken2012@yandex.ru>
2018-10-05 16:52:35 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,17 @@
+// NICE SCROLL
+jQuery(document).ready(function() {
+	jQuery("body").niceScroll({
+		cursorcolor:"#333",
+		cursorborder:"0px",
+		cursorwidth :"8px",
+		zindex:"9999"
+	});
+});
+// LINKS
+$("#service-block a").click(function(e) {
+
+    e.preventDefault();
+
+});
+
+
